e5b924673f2dbf80aa0de7e228d56253665161fa
Ellie575 <elliekubo@hotmail.com>
2022-06-20 11:13:36 +1000

    Added search function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Dictionary App</h1>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Code is opensourced and written by Ellie Kubo</footer>
       </div>
     </div>
   );
