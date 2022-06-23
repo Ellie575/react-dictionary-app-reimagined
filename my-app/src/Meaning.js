813f460d2635e8fdebf42e010419e938e11a2c55
Ellie575 <elliekubo@hotmail.com>
2022-06-23 15:22:43 +1000

    Added Example component

--- my-app/src/Meaning.js
+++ my-app/src/Meaning.js
@@ -1,21 +1,19 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import Examples from "./Examples";
 
 export default function Meaning(props) {
-  console.log(props);
   return (
     <div className="meaning">
       <h3>{props.meaning.partOfSpeech} </h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              <strong>Definition:</strong> {definition.definition}
-              <br />
-              <strong>Example:</strong> <em>{definition.example}</em>
-              <br />
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
+            <strong>Definition:</strong> {definition.definition}
+            <br />
+            <Examples examples={definition.example} />
+            <br />
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
